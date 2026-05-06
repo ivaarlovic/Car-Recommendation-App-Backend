ec8a1e0d76f7cb8d670632f36672e5884be69bc5
ivaarlovic <iva.arlovic03@gmail.com>
2026-05-06 17:12:47 +0200

    Allow frontend CORS

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+            policy.WithOrigins("https://ivaarlovic.github.io")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
