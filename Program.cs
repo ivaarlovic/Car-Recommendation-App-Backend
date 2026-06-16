a984bed99ae8b446be0154ba7271e4368de539c9
ivaarlovic <iva.arlovic03@gmail.com>
2026-06-16 19:07:35 +0200

    greska

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("https://ivaarlovic.github.io/car-recommendation-frontend")
+            policy.WithOrigins("https://ivaarlovic.github.io")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
