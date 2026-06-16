a57d73d6b82edfd7f1b09d442ef1dafc4f80def2
ivaarlovic <iva.arlovic03@gmail.com>
2026-06-16 19:17:29 +0200

    greska

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("https://ivaarlovic.github.io")
+            policy.WithOrigins("https://ivaarlovic.github.io", "http://localhost:5173")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
