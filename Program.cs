3f185f2e2c5f0130cc5e004a9145335b3155ea1c
ivaarlovic <iva.arlovic03@gmail.com>
2026-06-22 15:01:14 +0200

    greska

--- Program.cs
+++ Program.cs
@@ -1,45 +1,48 @@
-using CarRecommendationApp.Data;
+﻿using CarRecommendationApp.Data;
 using Microsoft.EntityFrameworkCore;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// === CORS - Bolja konfiguracija ===
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://127.0.0.1:5173",
+            "http://127.0.0.1:5174",
+            "https://ivaarlovic.github.io"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();   // važno za auth kasnije
+    });
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
         new MySqlServerVersion(new Version(8, 0, 0))
-        ));
-
+    ));
 
 builder.Services.AddControllers();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend",
-        policy =>
-        {
-            policy.AllowAnyHeader()
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-
-        });
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
+// === Middleware redoslijed JE BITAN ===
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
+}
 
-app.UseCors("AllowFrontend");
+app.UseCors("AllowFrontend"); 
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
