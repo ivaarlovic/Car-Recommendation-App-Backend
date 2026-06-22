e2e3b5acd39220d81a9739e1eeaae0dba51b6499
ivaarlovic <iva.arlovic03@gmail.com>
2026-06-22 14:53:50 +0200

    greska

--- Program.cs
+++ Program.cs
@@ -19,9 +19,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("https://ivaarlovic.github.io", "http://localhost:5173")
+            policy.AllowAnyHeader()
                   .AllowAnyHeader()
                   .AllowAnyMethod();
+
         });
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
